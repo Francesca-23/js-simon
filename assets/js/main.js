ab5e423f91ac90365a2986d018498989e8597dbc
Francesca-23 <francescadidomenico4@gmail.com>
2023-04-05 00:19:56 +0200

    Creazione struttura

---
+++ assets/js/main.js
@@ -0,0 +1,70 @@
+/*
+1-Visualizzare in pagina 5 numeri casuali. 
+    -funzione Math.random
+    -array vuoto per inserire i 5 num
+    -ciclo for per generare 5 num casuali nell'array
+    -stampare in pagina i 5 num
+
+2-Da lì parte un timer di 5 secondi. Dopo 5 secondi i numeri scompaiono
+    -setTimeout
+    -display-none dei 5 num in pagina: aggiungendo una classe
+
+3-l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+    -ciclo for per generare 5 prompt
+
+4-Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+    -if(array[num] == num inseriti nel prompt){
+        stampare il num
+    }
+*/
+
+
+//funzione per generare numeri randomici
+function random(){
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+
+//ciclo per stampare 5 numeri casuali nell'array vuoto
+let arrayNum = []
+
+for(let i = 0; arrayNum.length < 5; i++){
+    
+    arrayNum.push(random())
+    
+    document.querySelector('ul').innerHTML += `<li>${arrayNum[i]}</li>`
+}
+
+
+//funzione per far scomparire i 5 numeri dopo 3 secondi
+let timer = setTimeout(function(){
+    document.querySelector('ul').classList.add('d-none')
+}, 3000)
+
+
+
+
+let timer2 = setTimeout(function(){
+
+    let arrayPrompt = []
+
+    let numeriUtente
+
+    for(let i = 0; i < 5; i++){
+        numeriUtente = prompt('Inserisci il numero visualizzato')
+        arrayPrompt.push(numeriUtente)
+    }
+
+    // if(arrayNum[0] == arrayPrompt[0]){
+    //     console.log(numeriUtente)
+    // }
+    
+
+}, 4000)
+
+
+
+
+
+
+
